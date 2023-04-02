f2a52b7294f00ceaa9e00aee54d22a931dee8f56
DickySosa <127916303+DickySosa@users.noreply.github.com>
2023-04-02 15:02:51 -0600

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Import stylesheets
 import './style.css';
 
-/* Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`; */
+let boxes = document.querySelector('.wordle-layout-grid-container');
+
+function doMoreBoxes() {
+  console.log('si');
+  for (let i = 1; i <= 29; i++) {
+    const moreBoxes = document.createElement('input');
+    moreBoxes.setAttribute('class', 'box');
+    boxes.appendChild(moreBoxes);
+  }
+}
+doMoreBoxes();
