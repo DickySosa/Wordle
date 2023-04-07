fe445775576dae905f9ea10f273e5acf666da355
DickySosa <127916303+DickySosa@users.noreply.github.com>
2023-04-07 10:28:48 -0600

    2 files modified

--- index.js
+++ index.js
@@ -3,13 +3,38 @@ import './style.css';
 
 const letter = document.querySelectorAll('.scoreboard-letter');
 const loadingDiv = document.querySelector('.info.bar');
+const ANSWER_LENGTH = 5;
 
 async function init() {
+  const currentGuess = '';
+
+  function addLetter(letter) {
+    if (currentGuess.length < ANSWER_LENGTH) {
+      currentGuess += letter;
+    } else {
+      currentGuess = currentGuess.substring(0, currentGuess - 1 + letter);
+    }
+  }
+
   document.addEventListener('keydown', function handlekeyPress(event) {
     const action = event.key;
 
     console.log(action);
+
+    if (action === 'Enter') {
+      commit();
+    } else if (action === 'Backspace') {
+      backspace();
+    } else if (action === isLetter(action)) {
+      addLetter(action.toUpperCase());
+    } else {
+      // do nothing
+    }
   });
+}
+
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
 }
 init();
 /*
