a9c11a236efe10b30505348115e2f1521b5750ea
DickySosa <127916303+DickySosa@users.noreply.github.com>
2023-04-07 13:28:36 -0600

    3 files modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import './style.css';
 
 const letters = document.querySelectorAll('.scoreboard-letter');
-const loadingDiv = document.querySelector('.info.bar');
+const loadingDiv = document.querySelector('.info-bar');
 const ANSWER_LENGTH = 5;
 
 async function init() {
@@ -12,7 +12,7 @@ async function init() {
   const res = await fetch('https://words.dev-apis.com/word-of-the-day');
   const resObj = await res.json();
   const word = resObj.word.toUpperCase();
-
+  setLoading(false);
   console.log(word);
 
   function addLetter(letter) {
@@ -46,8 +46,6 @@ async function init() {
   document.addEventListener('keydown', function handlekeyPress(event) {
     const action = event.key;
 
-    console.log(action);
-
     if (action === 'Enter') {
       commit();
     } else if (action === 'Backspace') {
@@ -57,11 +55,13 @@ async function init() {
     } else {
       // do nothing
     }
-
-    function isLetter(letter) {
-      return /^[a-zA-Z]$/.test(letter);
-    }
   });
+}
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
+function setLoading(isLoading) {
+  loadingDiv.classList.toggle('show', isLoading);
 }
 
 init();
